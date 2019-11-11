ae7d611f60b06a3d783ccd73121ddf186b247a70
James Hibbard <1940994+jameshibbard@users.noreply.github.com>
2019-11-11 12:43:58 +0100

    Added preferences

--- content/options-dialog.js
+++ content/options-dialog.js
@@ -1,123 +1,72 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is MoreSnooze Extension code.
- *
- * The Initial Developer of the Original Code is
- * Cyrille Nocus
- * Portions created by the Initial Developer are Copyright (C) 2010
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+/* global ChromeUtils document */
+/* exported prefs */
 
-ChromeUtils.import("resource://gre/modules/Services.jsm");
+const { Services } = ChromeUtils.import('resource://gre/modules/Services.jsm');
 
-moreSnoozeChrome.initOptionsDialog = function() {
-  var defaultBranch = Services.prefs.getBranch("extensions.moresnooze.");
-  let moreSnoozeString = defaultBranch.getCharPref("snooze", moreSnoozeChrome.CURRENT_VERSION + '*' + JSON.stringify(moreSnoozeChrome.SNOOZE_LIST));
-    
-  moreSnoozeChrome.loadSnooze(moreSnoozeString);
-}
+class Preferences {
+  constructor() {
+    this.defaults = ['cb_30m', 'cb_01h', 'cb_03h', 'cb_01d', 'cb_03d', 'cb_01w'];
 
-moreSnoozeChrome.loadSnooze = function(moreSnoozeString) {
-  let obj;
-  
-  moreSnoozeString = moreSnoozeString.split('*');
-  if (!moreSnoozeChrome.in_array(moreSnoozeChrome.COMPATIBLES_VERSIONS, moreSnoozeString[0])){
-    moreSnoozeChrome.parsedSnooze = moreSnoozeChrome.SNOOZE_LIST;
-  } else {
-    try {
-      moreSnoozeChrome.parsedSnooze = JSON.parse(moreSnoozeString[1]);
-      if (moreSnoozeChrome.parsedSnooze.length == 0){moreSnoozeChrome.parsedSnooze = moreSnoozeChrome.SNOOZE_LIST;}
-    } catch (e) {
-      moreSnoozeChrome.parsedSnooze = moreSnoozeChrome.SNOOZE_LIST;
+    this.preferences = [
+      { id: 'extensions.moresnooze.cb_05m', type: 'bool' },
+      { id: 'extensions.moresnooze.cb_10m', type: 'bool' },
+      { id: 'extensions.moresnooze.cb_15m', type: 'bool' },
+      { id: 'extensions.moresnooze.cb_20m', type: 'bool' },
+      { id: 'extensions.moresnooze.cb_30m', type: 'bool' },
+      { id: 'extensions.moresnooze.cb_40m', type: 'bool' },
+      { id: 'extensions.moresnooze.cb_50m', type: 'bool' },
+      { id: 'extensions.moresnooze.cb_01h', type: 'bool' },
+      { id: 'extensions.moresnooze.cb_02h', type: 'bool' },
+      { id: 'extensions.moresnooze.cb_03h', type: 'bool' },
+      { id: 'extensions.moresnooze.cb_06h', type: 'bool' },
+      { id: 'extensions.moresnooze.cb_09h', type: 'bool' },
+      { id: 'extensions.moresnooze.cb_12h', type: 'bool' },
+      { id: 'extensions.moresnooze.cb_15h', type: 'bool' },
+      { id: 'extensions.moresnooze.cb_01d', type: 'bool' },
+      { id: 'extensions.moresnooze.cb_02d', type: 'bool' },
+      { id: 'extensions.moresnooze.cb_03d', type: 'bool' },
+      { id: 'extensions.moresnooze.cb_04d', type: 'bool' },
+      { id: 'extensions.moresnooze.cb_05d', type: 'bool' },
+      { id: 'extensions.moresnooze.cb_01w', type: 'bool' },
+      { id: 'extensions.moresnooze.cb_02w', type: 'bool' },
+    ];
+
+    this.branchName = 'extensions.moresnooze.';
+    this.branchNameLength = this.branchName.length;
+    this.branch = Services.prefs.getBranch(this.branchName);
+  }
+
+  init() {
+    for (const pref of this.preferences) {
+      const element = document.getElementById(pref.id);
+      const prefName = pref.id.substring(this.branchNameLength);
+
+      element.setAttribute('checked', this.branch.getBoolPref(prefName) ? 'true' : 'false');
+    }
+
+    document.addEventListener('dialogaccept', this.save);
+  }
+
+  save () {
+    for (const pref of this.preferences) {
+      const element = document.getElementById(pref.id);
+      const prefName = pref.id.substring(this.branchNameLength);
+
+      this.branch.setBoolPref(prefName, element.getAttribute('checked') == 'true');
     }
   }
 
-  for (var snooze  of Object.values(moreSnoozeChrome.parsedSnooze)) { 
-    if ((snooze.SL_id == undefined) || (snooze.SL_checked == undefined) || (snooze.SL_delay == undefined)){
-      // on écrase les ancienne prefs.
-      moreSnoozeChrome.parsedSnooze = moreSnoozeChrome.SNOOZE_LIST;
-    } else {
-      obj = document.getElementById(snooze.SL_id);
-      // si obj == null, on a des pref générées par une ancienne version
-      if (obj == null){
-		// on écrase les ancienne prefs.
-		moreSnoozeChrome.parsedSnooze = moreSnoozeChrome.SNOOZE_LIST;
-      } else {
-		if (snooze.SL_checked == "y"){
-		  obj.checked = true;
-		} else {
-		  obj.checked = false;
-		}
-      }
-    }
+  checkbox(operation) {
+    const checkboxes = document.querySelectorAll('checkbox');
+
+    if(operation === 'select') checkboxes.forEach(cb => cb.checked = true);
+    if(operation === 'deselect') checkboxes.forEach(cb => cb.checked = false);
+    if(operation === 'reload') checkboxes.forEach((cb) => {
+      cb.checked = this.defaults.includes(cb.id.replace('extensions.moresnooze.', '')) ? true : false;
+    });
+
+    this.save();
   }
 }
 
-moreSnoozeChrome.saveIfInstantApply = function() {
-  if (document.documentElement.instantApply) { 
-    moreSnoozeChrome.onSaveItems();
-  }
-}
-
-moreSnoozeChrome.onSaveItems = function(){
-
-  let snoozeArray = [];
-  let obj;
-
-  for (var snooze  of Object.values(moreSnoozeChrome.parsedSnooze)) {
-    obj = document.getElementById(snooze.SL_id);
-    if (obj.getAttribute("checked")){
-      snoozeArray.push({SL_id: snooze.SL_id, SL_checked: "y", SL_delay: snooze.SL_delay});
-    } else {
-      snoozeArray.push({SL_id: snooze.SL_id, SL_checked: "n", SL_delay: snooze.SL_delay});
-    }
-  }
-
-  var defaultBranch = Services.prefs.getBranch("extensions.moresnooze.");
-  defaultBranch.setCharPref("snooze", moreSnoozeChrome.CURRENT_VERSION + '*' + JSON.stringify(snoozeArray));
-}
-
-
-moreSnoozeChrome.ctrl_cb = function(fonction){
-  if (fonction == 'select'){
-    for (var snooze  of Object.values(moreSnoozeChrome.SNOOZE_LIST)){document.getElementById(snooze.SL_id).checked = true;}
-  }
-  if (fonction == 'deselect'){
-    for (var snooze  of Object.values(moreSnoozeChrome.SNOOZE_LIST)){document.getElementById(snooze.SL_id).checked = false;}
-  }
-  if (fonction == 'reload'){
-    for (var snooze  of Object.values(moreSnoozeChrome.SNOOZE_LIST)){
-      if (snooze.SL_checked == 'y'){document.getElementById(snooze.SL_id).checked = true;}
-	  if (snooze.SL_checked == 'n'){document.getElementById(snooze.SL_id).checked = false;}
-	}
-  }
-  if ((fonction == 'select') || (fonction == 'deselect') || (fonction == 'reload')){
-    moreSnoozeChrome.saveIfInstantApply();
-  }	
-}
+const prefs = new Preferences;
